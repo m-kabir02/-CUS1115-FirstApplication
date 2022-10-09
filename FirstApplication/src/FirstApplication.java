29f6ce3439f1947c1ea5b52b9edb96be49bf2ca3
Mosammad Kabir <97767575+m-kabir02@users.noreply.github.com>
2022-10-08 21:50:28 -0400

    Add files via upload
    CUS 1115 COMP PROG FUND I. First Java Program. Practice with Strings.

---
+++ FirstApplication/src/FirstApplication.java
@@ -0,0 +1,22 @@
+//Mosammad Kabir
+//2/1/2021
+//FirstApplication
+//Prints Hello Class!, Hello CUS 1115!, Today is Monday. It is snowing and Assignment ! is due by  Feb 4 by 9AM
+public class FirstApplication {
+    public static void main(String[] args)
+    {
+
+        System.out.print("Hello CLass!");
+        System.out.println("Hello CUS 1115!");
+        System.out.print("Today is Monday.\t It is snowing.\n\nOur next class is Thursday.");
+        System.out.println("Assignment 1 is due Feb 4 by 9AM");
+
+    }
+
+}
+/*
+Hello class! Hello CUS 1115!
+Today is Monday. It is snowing.
+
+ Our next class is Thursday. Assignment 1 is due Feb 4 by 9AM.
+ */
